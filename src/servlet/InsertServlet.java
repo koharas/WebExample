817cb97f0cbb8bdf2bb3dd4f49df09987928486b
edu00 <edu00@edupc00.n00d01.kis.co.jp>
2020-05-15 17:00:49 +0900

    0515

--- src/servlet/InsertServlet.java
+++ src/servlet/InsertServlet.java
@@ -13,16 +13,16 @@ import model.Shouhin;
 import model.ShouhinDAO;
 
 /**
- * Servlet implementation class ShouhinServlet
+ * Servlet implementation class InsertServlet
  */
-@WebServlet("/shouhin")
-public class ShouhinServlet extends HttpServlet {
+@WebServlet("/insert")
+public class InsertServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ShouhinServlet() {
+    public InsertServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -31,34 +31,34 @@ public class ShouhinServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
-		String sid = request.getParameter("sid");
-		int id = Integer.parseInt(sid);
 
-		ShouhinDAO dao = new ShouhinDAO();
-		Shouhin s = dao.findBySid(id);
-
-		response.setContentType("text/html; charset=UTF-8");
-		PrintWriter out = response.getWriter();
-
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>商品</title>");
-		out.println("</head>");
-		out.println("<body>");
-		out.println("<p>商品ID：" + s.getSid() + "</p>");
-		out.println("<p>商品名：" + s.getSname() + "</p>");
-		out.println("<p>単価：" + s.getTanka() + "円</p>");
-		out.println("</body>");
-		out.println("</html>");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		String sname = request.getParameter("sname");
+		String tankaStr = request.getParameter("tanka");
+		int tanka = Integer.parseInt(tankaStr);
+
+		ShouhinDAO dao = new ShouhinDAO();
+		Shouhin s1 = new Shouhin(0, sname,tanka);
+		dao.insert(s1);
+
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>追加</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>追加しました</p>");
+		out.println("<a href=\"slist\">商品一覧</a>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
