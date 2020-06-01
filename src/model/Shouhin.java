d446b076aae0fb66c9b80444e46d265dfb2fb9d2
edu00 <edu00@edupc00.n00d01.kis.co.jp>
2020-06-01 10:04:24 +0900

    0601

--- src/model/Shouhin.java
+++ src/model/Shouhin.java
@@ -1,6 +1,13 @@
 package model;
-
+/**
+ * 商品クラス。
+ * @author edu00
+ *
+ */
 public class Shouhin {
+	/**
+	 * 商品ID
+	 */
 	private int sid;
 	private String sname;
 	private int tanka;
@@ -9,12 +16,22 @@ public class Shouhin {
 
 	}
 
+	/**
+	 * コンストラクタ
+	 * @param sid 商品id
+	 * @param sname 商品名
+	 * @param tanka 単価
+	 */
 	public Shouhin(int sid, String sname, int tanka) {
 		this.sid = sid;
 		this.sname = sname;
 		this.tanka = tanka;
 	}
 
+	/**
+	 * 商品IDを取得
+	 * @return 商品ID
+	 */
 	public int getSid() {
 		return sid;
 	}
--- src/servlet/HelloServlet.java
+++ src/servlet/HelloServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/hello")
+@WebServlet("/hello/hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
